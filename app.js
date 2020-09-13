7c70037980ac3ec359e2e97e9fa770562a3ae9b7
Katherine <katherinehincapie19@hotmail.com>
2020-09-12 20:40:50 -0400

    con autenticación

--- app.js
+++ app.js
@@ -1,39 +1,100 @@
 var express = require('express');
+var app = express();
 const mongoose = require("mongoose");
 const Record = require("./models/record");
-var app = express();
-
-mongoose.connect("mongodb://localhost:27017/records", { useNewUrlParser: true });
+const User = require("./models/User");
+const cookieSession = require("cookie-session");
+const cookieParser = require("cookie-parser");
+mongoose.connect("mongodb://localhost:27017/records", { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.set("view engine", "pug");// configuraciòn para unir con pug
 app.set("views", "views"); //  decimos donde queremos guardar esos archivos
 app.use(express.urlencoded({extended: true}));
-app.use("/static", express.static("public"));
+app.use(cookieSession({
+  secret: "login",
+  maxAge: 24 * 60 * 60* 1000 // 24 hours
+}));
+app.use(cookieParser());
 
+//app.use("/static", express.static("public"));
+
+const requireUser = (req, res, next) => {
+  if (!res.locals.user) {
+    return res.redirect("/login");
+  }
+  next();
+};
+
+app.use(async (req, res, next) => {
+  const userId = req.session.userId;
+  if (userId)  {
+    const user = await User.findById(userId);
+    if (user) {
+        res.locals.user = user;
+    } else {
+      delete req.session.userId;
+    }
+  }
+  next();
+});
 
 app.get('/register', (req, res) => {
   res.render("new")
 });
 
 app.post('/register', async (req, res, next) => {
-  const data = {
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password
-  };
-
   try {
-    const record = new Record(data);
-    await record.save();
+    const user = await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password
+    });
+    res.redirect("/login");
   } catch(e) {
     return next(e);
   }
-  res.redirect("/");
 });
 
 app.get('/', async(req, res) => {
-  const records = await Record.find();
-  res.render("index", { records })
+  const users = await User.find();
+  const loggedIn = req.cookies["express:sess"];
+
+  res.render("index", { users, loggedIn })
 });
 
+app.get("/login", (req, res) =>{
+  const loggedIn = req.cookies["express:sess"];
+  res.render("login", { loggedIn });
+});
+
+app.post("/login", async (req, res) =>{
+  try {
+    const user = await User.authenticate(req.body.email, req.body.password);
+    if (user) {
+      req.session.userId = user.Id;
+      return res.redirect("/");
+    } else {
+      res.render("login", { error: "Wrong email or password. Try again!"});
+    }
+  } catch (err) {
+    return next(err);
+  }
+});
+
+app.get("/logout", (req, res) =>{
+  res.session = null;
+  res.clearCookie("express:sess");
+  res.clearCookie("express:sess.sig");
+  res.redirect("login");
+});
+
+
 app.listen(3000, () => console.log('Listening on port 3000!'));
+
+//Crear el modelo user (bcrypt)
+//GET /login
+//POST /login
+  //verificar si lo ingreso bien su usuario y contraseña, sisi guardamos en la sesiòn el id del usuario si no, volvemos a mostrar el formulario del login
+//GET /logout
+//Crear un middleware de autenticación
+//
